1b7648c46870e825d47d2111c8d8e0f00275c5b0
namhuynh2000 <hqnam.it@gmail.com>
2021-11-22 15:43:44 +0700

    UI
    Thiết kế giao diện add file/folder

---
+++ Dashboard/Dashboard1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+using Microsoft.Win32; //FileDialog
+using WinForms = System.Windows.Forms; //FolderDialog // To do this, add a reference to System.Windows.Forms under Project-> References with Add Reference.
+using System.IO; //Folder, Directory
+using System.Diagnostics; //Debug.WriteLine
+
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Dashboard1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(); //Để khi bấm vào icon shutdown, chương trình sẽ kết thúc
+        }
+
+        private void GridBarraTitulo_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove(); //Giúp duy chuyển cửa sổ hộp thoại
+        }
+
+        private void UploadFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true; //Giúp đọc được nhiểu file cùng lúc
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //Lọc qua mảng các file và lưu tên vào ListBox
+                foreach (string filename in openFileDialog.FileNames)
+                    dataListBoxFile.Items.Add(System.IO.Path.GetFileName(filename));
+            }
+        }
+
+        private void UploadFolder_Click(object sender, RoutedEventArgs e)
+        {
+            WinForms.FolderBrowserDialog folderDialog = new WinForms.FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == WinForms.DialogResult.OK)
+            {
+                //Lấy địa chỉ thư mục đã chọn
+                String sPath = folderDialog.SelectedPath;
+
+                //Lưu địa chỉ thư mục vào ListBox
+                dataListBoxFolder.Items.Add(sPath);
+            }
+        }
+    }
+}
