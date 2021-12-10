bda1d60b0a020a90a225636a08517a00958b2be9
HaruKishima <18120256@student.hcmus.edu.vn>
2021-12-10 14:10:36 +0700

    add extension rule

---
+++ Dashboard/RenameRuleLibrary/ExtensionRule.cs
@@ -0,0 +1,63 @@
+﻿using RenameRuleContract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenameRuleLibrary
+{
+    public class ExtensionRule : IRenameRule
+    {
+        public string Name => "Extension";
+        public string NewExtension { get; set; }
+
+        public ExtensionRule()
+        {
+            NewExtension = "";
+        }
+        public ExtensionRule(string newExtension)
+        {
+            NewExtension = newExtension;
+        }
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public string[] GetAllAttributesName()
+        {
+            return new string[] {"NewExtension"};
+        }
+
+        public object? GetAttribute(string key)
+        {
+            switch (key)
+            {
+                case "NewExtension":
+                    return NewExtension;
+                default:
+                    return null;
+            }
+        }
+
+        public void Rename(FileInfo original)
+        {
+            original.NewExtension = NewExtension;
+        }
+
+        public bool SetAttribute(string key, object value)
+        {
+            string strValue = (string)value;
+            switch (key)
+            {
+                case "NewExtension":
+                    NewExtension = strValue;
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+    }
+}
